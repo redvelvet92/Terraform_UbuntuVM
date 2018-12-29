5e0f532c0340f0f8cad8d9b95aee3561aaeebd68
redvelvet92 <tedmart92@gmail.com>
2018-12-28 23:09:45 -0600

    commit

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "azurerm_subnet" "subnet" {
 }
 
 resource "azurerm_network_security_group" "usnc-teds-nsg" {
-    name = "${var.network_security_group}"
+    name = "${var.prefix}-nsg"
     location = "${var.location}"
     resource_group_name = "${azurerm_resource_group.usnc-ubuntu-test.name}"
     
